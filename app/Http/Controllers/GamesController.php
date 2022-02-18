ef4c95a9254f93020d6f4712b935b8b3e330a624
Ilya Mur <ilyamur@hotmail.com>
2022-02-19 02:27:06 +0300

    finish show view

--- app/Http/Controllers/GamesController.php
+++ app/Http/Controllers/GamesController.php
@@ -43,12 +43,29 @@ class GamesController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  string  $slug
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show($slug)
     {
-        //
+        $game = Http::withHeaders(config('services.igdb.keys'))->withBody(
+            "
+            fields name, cover.url, first_release_date, platforms.abbreviation, rating,
+            slug, involved_companies.company.name, genres.name, aggregated_rating, summary, 
+            websites.*, videos.*, screenshots.*, similar_games.cover.url, similar_games.name, 
+            similar_games.rating,similar_games.platforms.abbreviation, similar_games.slug;
+            where slug=\"{$slug}\";
+            ",
+            'text/plain'
+        )->post(config('services.igdb.url'))->json();
+
+        abort_if(!$game, 404);
+
+        dump($game);
+        
+        return view('show', [
+            'game' => $game[0]
+        ]);
     }
 
     /**
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -4,22 +4,31 @@
     <div class="container mx-auto px-4">
         <div class="game-details border-b border-gray-800 pb-12 flex flex-col lg:flex-row">
             <div class="flex-none">
-                <img src="{{ asset('img/game.jpg') }}" alt="game cover">
+                @if(isset($game['cover']))
+                <img src="{{ Str::replaceFirst('thumb', 'cover_big', $game['cover']['url']) }}" alt="game cover"
+                    class="hover:opacity-75 transition ease-in-out duration-150">
+                @endif
             </div>
             <div class="lg:ml-12 lg:mr-64">
-                <h2 class="font-semibold text-4xl leading-tight mt-1">Duck Nuckem 3d</h2>
+                <h2 class="font-semibold text-4xl leading-tight mt-1">{{ $game['name'] }}</h2>
                 <div class="text-gray-400">
-                    <span>Adventure, RPG</span>
+                    <span> {{ implode(', ', array_column($game['genres'], 'name')) }}</span>
                     &middot;
-                    <span>Square Enix</span>
+                    <span> {{ $game['involved_companies'][0]['company']['name'] }}</span>
+
                     &middot;
-                    <span>Playstation 4</span>
+                    <span> {{ implode(', ', array_column($game['platforms'], 'abbreviation')) }}</span>
+
                 </div>
                 <div class="flex flex-wrap items-center mt-8">
                     <div class="flex items-center">
                         <div class="w-16 h-16 bg-gray-800 rounded-full">
                             <div class="font-semibold text-xs flex justify-center items-center h-full">
-                                90%
+                                @if (array_key_exists('rating', $game))
+                                    {{ round($game['rating']) . '%' }}
+                                @else
+                                    0%
+                                @endif
                             </div>
                         </div>
                         <div class="ml-4 text-xs">Member <br> Score</div>
@@ -28,7 +37,11 @@
                     <div class="flex items-center ml-12">
                         <div class="w-16 h-16 bg-gray-800 rounded-full">
                             <div class="font-semibold text-xs flex justify-center items-center h-full">
-                                92%
+                                @if (array_key_exists('aggregated_rating', $game))
+                                    {{ round($game['aggregated_rating']) . '%' }}
+                                @else
+                                    0%
+                                @endif
                             </div>
                         </div>
                         <div class="ml-4 text-xs">Critic <br> Score</div>
@@ -77,12 +90,11 @@
                     </div>
                 </div>
                 <p class="mt-12">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, velit! Vel nemo error esse nulla
-                    officiis libero ullam nesciunt fuga labore quo! Cum rem quam ut cupiditate atque error cumque?
+                    {{ $game['summary'] }}
                 </p>
 
                 <div class="mt-12">
-                    <button
+                    {{-- <button
                         class="flex bg-blue-500 text-white font-semibold px-4 py-4 hover:bg-blue-600 rounded transition ease-in-out duration-150">
                         <svg class="w-6 fill-current" viewBox="0 0 24 24">
                             <path d="M0 0h24v24H0z" fill="none"></path>
@@ -94,7 +106,20 @@
                             Play
                             Trailer
                         </span>
-                    </button>
+                    </button> --}}
+                    <a href="http://youtube.com/watch/{{ $game['videos'][0]['video_id'] }}"
+                        class="inline-flex bg-blue-500 text-white font-semibold px-4 py-4 hover:bg-blue-600 rounded transition ease-in-out duration-150">
+                        <svg class="w-6 fill-current" viewBox="0 0 24 24">
+                            <path d="M0 0h24v24H0z" fill="none"></path>
+                            <path
+                                d="M10 16.5l6-4.5-6-4.5v9zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z">
+                            </path>
+                        </svg>
+                        <span class="ml-2">
+                            Play
+                            Trailer
+                        </span>
+                    </a>
                 </div>
             </div>
 
@@ -104,42 +129,14 @@
                 Images
             </h2>
             <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 mt-8">
-                <div>
-                    <a href="#">
-                        <img src="/img/game.jpg" alt="screenshot"
-                            class="hover:opacity-75 transition ease-in-out duration-150">
-                    </a>
-                </div>
-                <div>
-                    <a href="#">
-                        <img src="/img/game.jpg" alt="screenshot"
-                            class="hover:opacity-75 transition ease-in-out duration-150">
-                    </a>
-                </div>
-                <div>
-                    <a href="#">
-                        <img src="/img/game.jpg" alt="screenshot"
-                            class="hover:opacity-75 transition ease-in-out duration-150">
-                    </a>
-                </div>
-                <div>
-                    <a href="#">
-                        <img src="/img/game.jpg" alt="screenshot"
-                            class="hover:opacity-75 transition ease-in-out duration-150">
-                    </a>
-                </div>
-                <div>
-                    <a href="#">
-                        <img src="/img/game.jpg" alt="screenshot"
-                            class="hover:opacity-75 transition ease-in-out duration-150">
-                    </a>
-                </div>
-                <div>
-                    <a href="#">
-                        <img src="/img/game.jpg" alt="screenshot"
-                            class="hover:opacity-75 transition ease-in-out duration-150">
-                    </a>
-                </div>
+                @foreach ($game['screenshots'] as $screenshot)
+                    <div>
+                        <a href="{{ Str::replaceFirst('thumb', 'screenshot_huge', $screenshot['url']) }}">
+                            <img src="{{ Str::replaceFirst('thumb', 'screenshot_big', $screenshot['url']) }}"
+                                alt="screenshot" class="hover:opacity-75 transition ease-in-out duration-150">
+                        </a>
+                    </div>
+                @endforeach
             </div>
         </div>
         <div class="images-container mt-8">
@@ -147,114 +144,31 @@
                 Similar games
             </h2>
             <div class="popular-games text-sm grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 xl:grid-cols-6 gap-12">
-                <div class="game mt-6">
-                    <div class="relative inline-block">
-                        <a href="#">
-                            <img src="/img/game.jpg" alt="game cover"
-                                class="hover:opacity-75 transition ease-in-out duration-150">
-                        </a>
-                        <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full"
-                            style="right:-20px; bottom: -20px">
-                            <div class="font-semibold text-xs flex justify-center items-center h-full">
-                                80%
-                            </div>
-                        </div>
-                    </div>
+                @foreach ($game['similar_games'] as $game)
+                    <div class="game mt-6">
+                        <div class="relative inline-block">
+                            <a href="#">
+                                <img src="{{ Str::replaceFirst('thumb', 'cover_big', $game['cover']['url']) }}"
+                                    alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
+                            </a>
+                            @if (isset($game['rating']))
+                                <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full"
+                                    style="right:-20px; bottom: -20px">
+                                    <div class="font-semibold text-xs flex justify-center items-center h-full">
+                                        {{ round($game['rating']) . '%' }}
+                                    </div>
+                                </div>
+                            @endif
 
-                    <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Duck Nuckem
-                        3D</a>
-                    <div class="text-gray-400 mt-1">Playstation 4</div>
-                </div>
-                <div class="game mt-6">
-                    <div class="relative inline-block">
-                        <a href="#">
-                            <img src="/img/game.jpg" alt="game cover"
-                                class="hover:opacity-75 transition ease-in-out duration-150">
-                        </a>
-                        <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full"
-                            style="right:-20px; bottom: -20px">
-                            <div class="font-semibold text-xs flex justify-center items-center h-full">
-                                80%
-                            </div>
                         </div>
-                    </div>
 
-                    <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Duck Nuckem
-                        3D</a>
-                    <div class="text-gray-400 mt-1">Playstation 4</div>
-                </div>
-                <div class="game mt-6">
-                    <div class="relative inline-block">
-                        <a href="#">
-                            <img src="/img/game.jpg" alt="game cover"
-                                class="hover:opacity-75 transition ease-in-out duration-150">
-                        </a>
-                        <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full"
-                            style="right:-20px; bottom: -20px">
-                            <div class="font-semibold text-xs flex justify-center items-center h-full">
-                                80%
-                            </div>
-                        </div>
+                        <a href="#"
+                            class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">{{ $game['name'] }}</a>
+                        <div class="text-gray-400 mt-1">
+                            {{ implode(', ', array_column($game['platforms'] ?? [], 'abbreviation')) }}</div>
                     </div>
+                @endforeach
 
-                    <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Duck Nuckem
-                        3D</a>
-                    <div class="text-gray-400 mt-1">Playstation 4</div>
-                </div>
-                <div class="game mt-6">
-                    <div class="relative inline-block">
-                        <a href="#">
-                            <img src="/img/game.jpg" alt="game cover"
-                                class="hover:opacity-75 transition ease-in-out duration-150">
-                        </a>
-                        <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full"
-                            style="right:-20px; bottom: -20px">
-                            <div class="font-semibold text-xs flex justify-center items-center h-full">
-                                80%
-                            </div>
-                        </div>
-                    </div>
-
-                    <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Duck Nuckem
-                        3D</a>
-                    <div class="text-gray-400 mt-1">Playstation 4</div>
-                </div>
-                <div class="game mt-6">
-                    <div class="relative inline-block">
-                        <a href="#">
-                            <img src="/img/game.jpg" alt="game cover"
-                                class="hover:opacity-75 transition ease-in-out duration-150">
-                        </a>
-                        <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full"
-                            style="right:-20px; bottom: -20px">
-                            <div class="font-semibold text-xs flex justify-center items-center h-full">
-                                80%
-                            </div>
-                        </div>
-                    </div>
-
-                    <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Duck Nuckem
-                        3D</a>
-                    <div class="text-gray-400 mt-1">Playstation 4</div>
-                </div>
-                <div class="game mt-6">
-                    <div class="relative inline-block">
-                        <a href="#">
-                            <img src="/img/game.jpg" alt="game cover"
-                                class="hover:opacity-75 transition ease-in-out duration-150">
-                        </a>
-                        <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full"
-                            style="right:-20px; bottom: -20px">
-                            <div class="font-semibold text-xs flex justify-center items-center h-full">
-                                80%
-                            </div>
-                        </div>
-                    </div>
-
-                    <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Duck Nuckem
-                        3D</a>
-                    <div class="text-gray-400 mt-1">Playstation 4</div>
-                </div>
             </div>
         </div>
     </div>
